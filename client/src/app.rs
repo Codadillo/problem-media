daa5d9fe338efbd838c0ab115a4bddbcf6fe9abc
Codadillo <leoconr@nuevaschool.org>
2020-04-13 18:28:05 -0700

    Set up router

--- client/src/app.rs
+++ client/src/app.rs
@@ -1,19 +1,63 @@
 use log::*;
 use yew::prelude::*;
+use yew_router::{route::Route, service::RouteService, Switch};
+
+#[derive(Debug, Switch, Clone)]
+pub enum AppRoute {
+    #[to = "/login"]
+    Login,
+}
+
+pub enum Msg {
+    RouteChanged(Route<()>),
+    ChangeRoute(AppRoute),
+}
 
 pub struct App {
+    route_service: RouteService<()>,
+    route: Route<()>,
     link: ComponentLink<Self>,
 }
 
+impl App {
+    fn change_route(&self, app_route: AppRoute) -> Callback<Event> {
+        self.link.callback(move |_| {
+            Msg::ChangeRoute(app_route.clone())
+        })
+    }
+}
+
 impl Component for App {
-    type Message = ();
+    type Message = Msg;
     type Properties = ();
 
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        App { link }
+        let mut route_service: RouteService<()> = RouteService::new();
+        let route = route_service.get_route();
+        let callback = link.callback(Msg::RouteChanged);
+        route_service.register_callback(callback);
+
+        App {
+            route_service,
+            route,
+            link,
+        }
     }
 
-    fn update(&mut self, _: Self::Message) -> ShouldRender {
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::RouteChanged(route) => self.route = route,
+            Msg::ChangeRoute(route) => {
+                let route_string = match route {
+                    AppRoute::Login => "/login".to_string()
+                };
+                self.route_service.set_route(&route_string, ());
+                self.route = Route {
+                    route: route_string,
+                    state: (),
+                };
+            }
+        }
         true
     }
 
