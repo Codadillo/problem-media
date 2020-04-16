2d6fada1541d6eb482b3519d4161182dc3949bb9
Codadillo <leoconr@nuevaschool.org>
2020-04-15 19:04:51 -0700

    Split up free response solutions and restrictions and added check methods

--- common/src/problems.rs
+++ common/src/problems.rs
@@ -30,7 +30,8 @@ pub enum Topic {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum ProblemContent {
     FreeResponse {
-        restrictions: Vec<FreeResponseSolution>,
+        restrictions: Vec<FreeResponseRestriction>,
+        solution: Vec<FreeResponseSolution>,
     },
     MultipleChoice {
         options: Vec<String>,
@@ -54,6 +55,7 @@ impl ProblemContent {
         match self {
             ProblemContent::FreeResponse {
                 restrictions: _,
+                solution: _,
             } => ProblemType::FreeResponse,
             ProblemContent::MultipleChoice {
                 options: _,
@@ -68,12 +70,11 @@ impl ProblemContent {
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum FreeResponseSolution {
-    // These are restrictions users see
+pub enum FreeResponseRestriction {
     Imaginary,
     Integer,
     Natural,
-    MaxCharacterLength(u32),
+    MaxCharacterLength(usize),
     RealInRange {
         start: Option<f64>,
         end: Option<f64>,
@@ -82,16 +83,65 @@ pub enum FreeResponseSolution {
         start: Option<f64>,
         end: Option<f64>,
     },
-    // These determine whether or not a solution is correct
-    RealEquals {
-        eq: f64,
-        precision: f64,
-    },
-    ImaginaryEquals {
-        eq: f64,
-        precision: f64,
-    },
-    TextEquals {
-        eq: String,
-    },
+}
+
+impl FreeResponseRestriction {
+    /// Checks if a given input passes the restriction.
+    /// returns true if the response is valid.
+    pub fn check(&self, response: String) -> bool {
+        match self {
+            FreeResponseRestriction::Imaginary => {
+                unimplemented!("Imaginary numbers not yet supported as a solution")
+            }
+            FreeResponseRestriction::Integer => response.parse::<i32>().is_ok(),
+            FreeResponseRestriction::Natural => response.parse::<u32>().is_ok(),
+            FreeResponseRestriction::MaxCharacterLength(length) => response.len() < *length,
+            FreeResponseRestriction::RealInRange { start, end } => match response.parse::<f64>() {
+                Ok(num) => {
+                    if let Some(start) = start {
+                        if num < *start {
+                            return false;
+                        }
+                    }
+                    if let Some(end) = end {
+                        if num > *end {
+                            return false;
+                        }
+                    }
+                    true
+                }
+                Err(_) => false,
+            },
+            FreeResponseRestriction::ImaginaryInRange { start: _, end: _ } => {
+                unimplemented!("Imaginary numbers not yet supported as a solution")
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum FreeResponseSolution {
+    RealEquals { eq: f64, precision: f64 },
+    ImaginaryEquals { eq: f64, precision: f64 },
+    TextEquals { eq: String },
+}
+
+impl FreeResponseSolution {
+    /// Checks if a given input is the solution.
+    /// returns true if the response is valid.
+    pub fn check(&self, response: String) -> bool {
+        match self {
+            FreeResponseSolution::RealEquals { eq, precision } => {
+                if let Ok(num) = response.parse::<f64>() {
+                    (num - eq).abs() < *precision
+                } else {
+                    false
+                }
+            }
+            FreeResponseSolution::ImaginaryEquals { eq, precision } => {
+                unimplemented!("Imaginary numbers not yet supported as a solution")
+            }
+            FreeResponseSolution::TextEquals { eq } => &response == eq,
+        }
+    }
 }
