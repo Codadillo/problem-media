9b4ec71713076be9d86fdd7afaeb92863b83ff3c
Codadillo <leoconr@nuevaschool.org>
2020-04-17 18:50:22 -0700

    Changed name in menu bar

--- client/src/app.rs
+++ client/src/app.rs
@@ -134,7 +134,7 @@ impl Component for App {
                             <div class="app">
                                 <nav class="menu">
                                     <span class="title">
-                                        { "AKSHAR" }
+                                        { "Enλgma" }
                                     </span>
                                     <div class="routes">
                                         <div class="createroute">
